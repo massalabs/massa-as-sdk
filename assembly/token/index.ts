b3d47927baac86eb2ae5077c28f4d2a5e2587a37
EvgeniPirianov <evgeni.pirianov@level01.io>
2022-08-23 10:45:06 +0300

    proper exports

--- assembly/token/index.ts
+++ assembly/token/index.ts
@@ -1,5 +1,10 @@
+/*
 import * as StandardToken from './std/impl';
 import {TokenWrapper} from './std/wrapper';
 
 export {StandardToken};
 export {TokenWrapper};
+*/
+
+export * from './std/impl';
+export * from './std/wrapper';
