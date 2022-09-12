b3d8a4f7c0fab5a93ea2d8c10e2fb4c89359f1ac
EvgeniPirianov <evgeni.pirianov@level01.io>
2022-09-13 00:31:49 +0300

    added serde

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,5 +3,6 @@ export * from './token/index';
 export * from './types/index';
 export * from './helpers/probability/index';
 export * from './collections/persistentMap';
+export * from './serde/index';
 
 
