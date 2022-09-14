6d92c58d9bbe54be71eb9935f7bc644cb7a94f90
EvgeniPirianov <evgeni.pirianov@level01.io>
2022-09-14 11:15:17 +0300

    reworked export

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,6 +1,7 @@
 // Standard Massa AssemblyScript SDK
 export * from './token/index';
 export * from './collections/persistentMap';
-export * as std from '@massalabs/massa-sc-std/assembly';
+import * as std from '@massalabs/massa-sc-std/assembly';
+export {std};
 
 
