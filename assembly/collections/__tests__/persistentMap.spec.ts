9521d696491f480b777368c578e06e3c490d949a
Nathan Seva <thykof@protonmail.ch>
2022-10-25 16:28:23 +0200

    remove failing test

--- assembly/collections/__tests__/persistentMap.spec.ts
+++
@@ -1,9 +0,0 @@
-import {PersistentMap} from '../persistentMap';
-
-describe('Persistent Map tests', () => {
-  it('empty map', () => {
-    const map = new PersistentMap<string, string>('pre');
-    map.set('foo', 'var');
-    assert(map.get('foo') === 'var', 'wrong value');
-  });
-});
