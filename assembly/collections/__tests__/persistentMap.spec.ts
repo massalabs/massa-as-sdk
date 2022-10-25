2abaf7a3660824b1f5d8101cbeee614912985a25
Nathan Seva <thykof@protonmail.ch>
2022-10-25 15:34:29 +0200

    fix test

--- assembly/collections/__tests__/persistentMap.spec.ts
+++ assembly/collections/__tests__/persistentMap.spec.ts
@@ -2,7 +2,8 @@ import {PersistentMap} from '../persistentMap';
 
 describe('Persistent Map tests', () => {
   it('empty map', () => {
-    const map = new PersistentMap();
-    console.log(map);
+    const map = new PersistentMap<string, string>('pre');
+    map.set('foo', 'var');
+    assert(map.get('foo') === 'var', 'wrong value');
   });
 });
