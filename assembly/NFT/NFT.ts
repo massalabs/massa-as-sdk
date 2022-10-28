c6887f46101fc8e974bdbd188a6853153b70d740
Sebastien Valla <83454858+SebastienValla@users.noreply.github.com>
2022-10-28 10:49:16 +0200

    resolve compiling issues

--- assembly/NFT/NFT.ts
+++ assembly/NFT/NFT.ts
@@ -7,36 +7,35 @@
 ///////////////////////////////
 
 import {
-  Address,
-  Storage,
-  Context,
-  generateEvent,
-} from "@massalabs/massa-as-sdk";
-import { allowance } from "@massalabs/massa-as-sdk/assembly/token";
+    Address,
+    Storage,
+    Context,
+    generateEvent,
+} from '@massalabs/massa-as-sdk';
 
-const nameKey: string = "Name";
-const symbolKey: string = "Symbol";
-const ownersKey: string = "Owners";
-const counterKey: string = "Counter";
-const ownerKey: string = "Owner";
-const maxSupplyKey: string = "MaxSupply";
-const baseURIKey: string = "baseURI";
+const nameKey: string = 'Name';
+const symbolKey: string = 'Symbol';
+const ownersKey: string = 'Owners';
+const counterKey: string = 'Counter';
+const ownerKey: string = 'Owner';
+const maxSupplyKey: string = 'MaxSupply';
+const baseURIKey: string = 'baseURI';
 const initCounter: u64 = 0;
 let arrOwners: Array<string>;
 
-const name: string = "MASSA_NFT";
-const symbol: string = "NFT";
+const name: string = 'MASSA_NFT';
+const symbol: string = 'NFT';
 const maxSupply: u32 = 10000;
-const baseURI: string = "massa.net/nft/";
+const baseURI: string = 'massa.net/nft/';
 
 arrOwners = new Array<string>(maxSupply * 2);
 for (let i = 0; i < arrOwners.length; ++i) {
-  arrOwners[i] = "";
+    arrOwners[i] = '';
 }
 let j = 1;
 for (let i = 1; i < arrOwners.length; i += 2) {
-  arrOwners[i] = j.toString();
-  j++;
+    arrOwners[i] = j.toString();
+    j++;
 }
 /**
  * Increment the NFT counter
@@ -45,8 +44,8 @@ for (let i = 1; i < arrOwners.length; i += 2) {
  */
 
 function increment(_: string): void {
-  const incr = u64(parseInt(Storage.get(counterKey))) + 1;
-  Storage.set(counterKey, incr.toString());
+    const incr = u64(parseInt(Storage.get(counterKey))) + 1;
+    Storage.set(counterKey, incr.toString());
 }
 
 /**
@@ -62,22 +61,22 @@ function increment(_: string): void {
  */
 
 export function setNFT(
-  name: string,
-  symbol: string,
-  maxSupply: u64,
-  baseURI: string
+    name: string,
+    symbol: string,
+    maxSupply: u64,
+    baseURI: string
 ): void {
-  assert(Storage.has(nameKey), "NFT already setted");
-  Storage.set(nameKey, name);
-  Storage.set(symbolKey, symbol);
-  Storage.set(maxSupplyKey, maxSupply.toString());
-  Storage.set(baseURIKey, baseURI);
-  Storage.set(ownerKey, Context.caller()._value);
-  Storage.set(ownersKey, arrOwners.toString());
-  Storage.set(counterKey, initCounter.toString());
-  generateEvent(
-    `${name} with symbol  ${symbol} and total supply of  ${maxSupply.toString()} is well setted`
-  );
+    assert(Storage.has(nameKey), 'NFT already setted');
+    Storage.set(nameKey, name);
+    Storage.set(symbolKey, symbol);
+    Storage.set(maxSupplyKey, maxSupply.toString());
+    Storage.set(baseURIKey, baseURI);
+    Storage.set(ownerKey, Context.caller()._value);
+    Storage.set(ownersKey, arrOwners.toString());
+    Storage.set(counterKey, initCounter.toString());
+    generateEvent(
+        `${name} with symbol  ${symbol} and total supply of  ${maxSupply.toString()} is well setted`
+    );
 }
 
 /**
@@ -87,7 +86,7 @@ export function setNFT(
  */
 
 export function Name(_: string): string {
-  return name;
+    return name;
 }
 /**
  * Return the NFT's symbol
@@ -96,7 +95,7 @@ export function Name(_: string): string {
  */
 
 export function Symbol(_: string): string {
-  return symbol;
+    return symbol;
 }
 
 /**
@@ -106,7 +105,7 @@ export function Symbol(_: string): string {
  */
 
 export function TokenURI(tokenId: u64): string {
-  return baseURI + tokenId.toString();
+    return baseURI + tokenId.toString();
 }
 
 /**
@@ -116,7 +115,7 @@ export function TokenURI(tokenId: u64): string {
  */
 
 export function BaseURI(_: string): string {
-  return baseURI;
+    return baseURI;
 }
 
 /**
@@ -126,7 +125,7 @@ export function BaseURI(_: string): string {
  */
 
 export function LimitSupply(_: string): u64 {
-  return u64(parseInt(Storage.get(maxSupplyKey)));
+    return u64(parseInt(Storage.get(maxSupplyKey)));
 }
 
 /**
@@ -136,7 +135,7 @@ export function LimitSupply(_: string): u64 {
  */
 
 export function CurrentSupply(_: string): u32 {
-  return u32(parseInt(Storage.get(counterKey)));
+    return u32(parseInt(Storage.get(counterKey)));
 }
 
 /**
@@ -147,7 +146,7 @@ export function CurrentSupply(_: string): u32 {
  */
 
 export function CheckLedger(_: string): Array<string> {
-  return Storage.get(ownersKey).split(",");
+    return Storage.get(ownersKey).split(',');
 }
 
 /**
@@ -158,7 +157,7 @@ export function CheckLedger(_: string): Array<string> {
  */
 
 export function OnlyOwner(_: string): bool {
-  return Context.caller()._value == Storage.get(ownerKey);
+    return Context.caller()._value == Storage.get(ownerKey);
 }
 
 /**
@@ -169,11 +168,11 @@ export function OnlyOwner(_: string): bool {
  */
 
 export function OwnerIndex(address: Address): Array<u64> {
-  const arrRaw = CheckLedger("_");
-  let indexes: Array<u64> = [];
-  for (let i = 0; i < arrRaw.length; i++)
-    if (arrRaw[i] === address._value) indexes.push(i + 1);
-  return indexes;
+    const arrRaw = CheckLedger('_');
+    const indexes: Array<u64> = [];
+    for (let i = 0; i < arrRaw.length; i++)
+        if (arrRaw[i] === address._value) indexes.push(i + 1);
+    return indexes;
 }
 
 /**
@@ -184,9 +183,9 @@ export function OwnerIndex(address: Address): Array<u64> {
  */
 
 export function OwnerOf(tokenId: u64): string {
-  const arrRaw = CheckLedger("_");
-  const OwnerIndex = arrRaw.indexOf(tokenId.toString()) - 1;
-  return arrRaw[OwnerIndex];
+    const arrRaw = CheckLedger('_');
+    const OwnerIndex = arrRaw.indexOf(tokenId.toString()) - 1;
+    return arrRaw[OwnerIndex];
 }
 
 /**
@@ -198,35 +197,35 @@ export function OwnerOf(tokenId: u64): string {
  */
 
 export function Mint(to: Address): void {
-  assert(LimitSupply("_") >= CurrentSupply("_"), "Limit supply reached");
-  const arrRaw = CheckLedger("_");
-  arrRaw[CurrentSupply("_") + 1] = to._value;
-  Storage.set(ownersKey, arrRaw.join(","));
-  increment("_");
-  generateEvent(
-    `tokenId ${CurrentSupply("_").toString()} minted to ${to._value} `
-  );
+    assert(LimitSupply('_') >= CurrentSupply('_'), 'Limit supply reached');
+    const arrRaw = CheckLedger('_');
+    arrRaw[CurrentSupply('_') + 1] = to._value;
+    Storage.set(ownersKey, arrRaw.join(','));
+    increment('_');
+    generateEvent(
+        `tokenId ${CurrentSupply('_').toString()} minted to ${to._value} `
+    );
 }
 
 /**
-   *Transfer a choosen token from the caller to the to Address
-   check first the caller owns the token 
-   * @param {Address} to
-   * @param {u64} tokenId
-   * @return {void}
-   */
+ *Transfer a choosen token from the caller to the to Address
+ check first the caller owns the token 
+ * @param {Address} to
+ * @param {u64} tokenId
+ * @return {void}
+ */
 
 export function Tranfer(to: Address, tokenId: u64): void {
-  assert(
-    OwnerOf(tokenId) == Context.caller()._value,
-    `You are not the owner of ${tokenId.toString()}`
-  );
-  const arrRaw = CheckLedger("_");
-  arrRaw[arrRaw.indexOf(tokenId.toString()) - 1] = to._value;
-  Storage.set(ownersKey, arrRaw.join(","));
-  generateEvent(
-    `token ${tokenId.toString()} sent from ${Context.caller()._value} to ${
-      to._value
-    }`
-  );
+    assert(
+        OwnerOf(tokenId) == Context.caller()._value,
+        `You are not the owner of ${tokenId.toString()}`
+    );
+    const arrRaw = CheckLedger('_');
+    arrRaw[arrRaw.indexOf(tokenId.toString()) - 1] = to._value;
+    Storage.set(ownersKey, arrRaw.join(','));
+    generateEvent(
+        `token ${tokenId.toString()} sent from ${Context.caller()._value} to ${
+            to._value
+        }`
+    );
 }
