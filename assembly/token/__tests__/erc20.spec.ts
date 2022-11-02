1e8b8f5e2987fa9550163a97d6cfd5f0c20e043e
Nathan Seva <thykof@protonmail.ch>
2022-11-02 15:42:24 +0100

    uncomment a test

--- assembly/token/__tests__/erc20.spec.ts
+++ assembly/token/__tests__/erc20.spec.ts
@@ -1,26 +1,24 @@
-// import * as token from '../impl';
+import * as token from '../impl';
 // TODO change relative path to cleaner import
 // import {setData} from '../../vm-mock/storage';
 
-/*
 describe('Black box tests', () => {
   it('should expose token name', () => {
-    expect<string>(token.name('')).toBe('Massa ERC20 token');
+    expect<string>(token.name('')).toBe('Standard token implementation');
   });
 
-  it('should return 0 for initialized balance', () => {
-    expect<string>(token.balanceOf('XXXaddress-1XXX')).toBe(
-        '0',
-        'default balance not working'
-    );
-  });
+  //   it('should return 0 for initialized balance', () => {
+  //     expect<string>(token.balanceOf('XXXaddress-1XXX')).toBe(
+  //       '0',
+  //       'default balance not working',
+  //     );
+  //   });
 
-  it('should return initialized balance', () => {
-    setData('balXXXaddress-1XXX', '1000');
-    expect<string>(token.balanceOf('XXXaddress-1XXX')).toBe(
-        '1000',
-        'initialized balance not working'
-    );
-  });
+  //   it('should return initialized balance', () => {
+  //     setData('balXXXaddress-1XXX', '1000');
+  //     expect<string>(token.balanceOf('XXXaddress-1XXX')).toBe(
+  //       '1000',
+  //       'initialized balance not working',
+  //     );
+  //   });
 });
-*/
