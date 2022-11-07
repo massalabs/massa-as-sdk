a1e76b13c01ae4d739371d510b5cd8e7c1cae7cc
Sebastien Valla <83454858+SebastienValla@users.noreply.github.com>
2022-11-07 13:28:59 +0100

    add comments to NFTfortest

--- assembly/NFT/NFT_fortest.ts
+++ assembly/NFT/NFT_fortest.ts
@@ -6,12 +6,21 @@
 ////////////////////////////
 ///////////////////////////////
 
+// as astester.imports.js for the
+//  moment only contents setOf, getOf and generateEvent,
+// this NFT_fortest contract has been modificated in consequences
+// original contract kept commented to further implementation
+
 import {Address, Storage, Context, generateEvent} from '../std';
+import {ByteArray} from '@massalabs/as/assembly';
 const ownerTokenKey: string = 'ownerOf_';
 const counterKey: string = 'Counter';
 const ownerKey: string = 'Owner';
 const baseURIKey: string = 'baseURI';
 const initCounter: u64 = 0;
+
+const ofAddress = new Address('0x');
+const OwnerAddress = new Address('9x');
 
 /**
  *The NFT's main characteristics
@@ -21,39 +30,47 @@ const name: string = 'MASSA_NFT';
 const symbol: string = 'NFT';
 const maxSupply: string = '3';
 const baseURI: string = 'massa.net/nft/';
-const ofAddress = new Address('0x');
-
-/**
- * Increment the NFT counter
- * @param {string} _ - unused see https://github.com/massalabs/massa-sc-std/issues/18
- * @return {string}
- */
-
-export function increment(_: string): void {
-    const incr = u64(parseInt(Storage.getOf(ofAddress, counterKey))) + 1;
-    Storage.setOf(ofAddress, counterKey, incr.toString());
-}
 
 /**
  * Init the NFT with name, symbol, maxsupply and baseURI,
  * init the counter to 0, the owner of the contract,
- * the "ledger" of owner of token IDs ,
- * then create key value for each.
  * @param {string} _ - unused see https://github.com/massalabs/massa-sc-std/issues/18
  * @return {string}
  */
 
 export function setNFT(_: string): string {
-    if (!Storage.has(counterKey)) {
-        Storage.setOf(ofAddress, baseURIKey, baseURI);
-        //  Storage.setOf(ofAddress, ownerKey, Context.caller()._value);
-        Storage.setOf(ofAddress, counterKey, initCounter.toString());
-        generateEvent(
-            `${name} with symbol  ${symbol} and total supply of  ${maxSupply} is well setted`
-        );
+    //  if (!Storage.has(counterKey)) {
+    Storage.setOf(ofAddress, baseURIKey, baseURI);
+    Storage.setOf(ofAddress, ownerKey, OwnerAddress.toByteString());
+    Storage.setOf(ofAddress, counterKey, initCounter.toString());
+    generateEvent(
+        `${name} with symbol  ${symbol} and total supply of  ${maxSupply} is well setted`
+    );
+    // } else {
+    //     generateEvent(`NFT already setted`);
+    // }
+    return '';
+}
+
+/**
+ * Change the base URI, can be only called by the contract Owner
+ * @param {string} newBaseURI new link include in the NFTs
+ * @return {string}
+ */
+
+export function SetURI(newBaseURI: string): string {
+    if (_OnlyOwner('')) {
+        Storage.setOf(ofAddress, baseURIKey, newBaseURI);
+        generateEvent(`new base URI ${newBaseURI} well setted`);
+    } else {
+        generateEvent(`you are not the contract Owner`);
     }
     return '';
 }
+
+// ======================================================== //
+// ====                 TOKEN ATTIBUTES                ==== //
+// ======================================================== //
 
 /**
  * Return the NFT's name
@@ -111,8 +128,67 @@ export function LimitSupply(_: string): string {
  */
 
 export function CurrentSupply(_: string): string {
+    // if (Storage.hasOf(ofAddress, counterKey)) {
     return Storage.getOf(ofAddress, counterKey);
+    // } else {
+    //     generateEvent(`NFT not setted yet`);
+    //     return "";
+    // }
 }
+
+/**
+ *
+ * Return the tokenId's owner
+ * @param {string} tokenId
+ * @return {string}
+ */
+
+export function OwnerOf(tokenId: string): string {
+    // if (Storage.hasOf(ofAddress, ownerTokenKey + tokenId.toString())) {
+    return Storage.getOf(ofAddress, ownerTokenKey + tokenId.toString());
+    // } else {
+    //     generateEvent(`NFT not minted yet`);
+    //     return "";
+    // }
+}
+
+// ==================================================== //
+// ====                 TRANSFER                   ==== //
+// ==================================================== //
+/**
+ *
+ * The to address becomes the owner of the next token (if current tokenID = 10, will mint 11 )
+ * Check if max supply is not reached
+ * @param {string} args - byte string containing an owner's account (Address).
+ * @return {string}
+ */
+
+export function Mint(args: string): string {
+    if (u32(parseInt(LimitSupply(''))) > u32(parseInt(CurrentSupply('')))) {
+        const addr = Address.fromByteString(args);
+        _increment('');
+        const tokenID: string = CurrentSupply('');
+        const key = ownerTokenKey + tokenID;
+        Storage.setOf(ofAddress, key, addr.toByteString());
+        generateEvent(`tokenId ${tokenID} minted to ${addr.toByteString()} `);
+    } else {
+        generateEvent(`Max supply reached`);
+    }
+    return 'Minted';
+}
+
+/**
+ * Increment the NFT counter
+ * @param {string} _ - unused see https://github.com/massalabs/massa-sc-std/issues/18
+ * @return {string}
+ */
+function _increment(_: string): string {
+    const add1: u32 = 1;
+    const incr = u32(parseInt(Storage.getOf(ofAddress, counterKey))) + add1;
+    Storage.setOf(ofAddress, counterKey, incr.toString());
+    return '';
+}
+
 /**
  *
  * Return true if the caller is the creator of the SC
@@ -120,54 +196,46 @@ export function CurrentSupply(_: string): string {
  *  * @return {bool}
  */
 
-export function OnlyOwner(_: string): bool {
-    return Context.caller()._value == Storage.getOf(ofAddress, ownerKey);
+//always true for test
+function _OnlyOwner(_: string): bool {
+    return '9x' == Storage.getOf(ofAddress, ownerKey);
 }
 
+// ==================================================== //
+// ====                 TRANSFER                   ==== //
+// ==================================================== //
+
 /**
- *
- * Return the owner of a tokenID
- * @param {u64} tokenId
+ *Transfer a choosen token from the caller to the to Address
+ check first the caller owns the token and if token minted
+ * @param {string} args - byte string with the following format:
+ * - the recipient's account (address)
+ * - the tokenID (u64).
  * @return {string}
  */
 
-export function OwnerOf(tokenId: u64): string {
-    return Storage.getOf(ofAddress, ownerTokenKey + tokenId.toString());
-}
-/**
- *
- * The to address becomes the owner of the next token (if current tokenID = 10, will mint 11 )
- * Check if max supply is not reached
- * @param {Address} to
- * @return {void}
- */
+export function Transfer(args: string): string {
+    const toAddress = new Address();
+    const offset = toAddress.fromStringSegment(args);
 
-export function Mint(to: Address): void {
-    if (u32(parseInt(LimitSupply('_'))) > u32(parseInt(CurrentSupply('_')))) {
-        increment('_');
-        Storage.setOf(ofAddress, ownerTokenKey + CurrentSupply(''), to._value);
-        generateEvent(`tokenId ${CurrentSupply('_')} minted to ${to._value} `);
-    } else {
-        generateEvent(`Max supply reached`);
-    }
-}
-/**
- *Transfer a choosen token from the caller to the to Address
- check first the caller owns the token 
- * @param {Address} to
- * @param {u64} tokenId
- * @return {void}
- */
-
-export function Tranfer(to: Address, tokenId: u64): string {
-    if (OwnerOf(tokenId) == '1x') {
-        Storage.setOf(ofAddress, ownerTokenKey + tokenId.toString(), to._value);
-        generateEvent(
-            `token ${tokenId.toString()} sent from 1x
-            } to ${to._value}`
-        );
-    } else {
-        `You are not the owner of ${tokenId.toString()}`;
-    }
+    const tokenId: u64 = ByteArray.fromByteString(
+        args.substr(offset, 8)
+    ).toU64();
+    // if (!Storage.hasOf(ofAddress, ownerTokenKey + tokenId.toString())) {
+    //     generateEvent(`token ${tokenId.toString()} not yet minted`);
+    //     return "";
+    // }
+    // if (OwnerOf(tokenId.toString()) == Context.caller().toByteString()) {
+    Storage.setOf(
+        ofAddress,
+        ownerTokenKey + tokenId.toString(),
+        toAddress.toByteString()
+    );
+    generateEvent(
+        `token ${tokenId.toString()} sent from you to ${toAddress.toByteString()}`
+    );
+    // } else {
+    //     generateEvent(`You are not the owner of ${tokenId.toString()}`);
+    // }
     return '';
 }
