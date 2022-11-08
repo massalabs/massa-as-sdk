c6997f79d2ddbe377c11376e04aeb40ed9818327
Nathan Seva <thykof@protonmail.ch>
2022-11-08 09:18:52 +0100

    format code

--- assembly/NFT/NFTWrapper.ts
+++ assembly/NFT/NFTWrapper.ts
@@ -23,10 +23,10 @@ export class NFTWrapper {
   _limitSupply: string;
 
   /**
-     * Wraps a smart contract exposing standard token FFI.
-     *
-     * @param {Address} at - Address of the smart contract.
-     */
+   * Wraps a smart contract exposing standard token FFI.
+   *
+   * @param {Address} at - Address of the smart contract.
+   */
   constructor(at: Address) {
     this._origin = at;
     this._name = call(this._origin, 'name', '', 0);
@@ -37,70 +37,70 @@ export class NFTWrapper {
   }
 
   /**
-     * Return the NFT's name
-     * @return {string}
-     */
+   * Return the NFT's name
+   * @return {string}
+   */
   name(): string {
     return this._name;
   }
   /**
-     * Return the NFT's symbol
-     * @return {string}
-     */
+   * Return the NFT's symbol
+   * @return {string}
+   */
   symbol(): string {
     return this._symbol;
   }
 
   /**
-     * Return the token URI (external link written in NFT where pictures or others a stored)
-     * @param {u64} tokenId
-     * @return {string}
-     */
+   * Return the token URI (external link written in NFT where pictures or others a stored)
+   * @param {u64} tokenId
+   * @return {string}
+   */
   tokenURI(tokenId: u64): string {
     return this._baseURI + tokenId.toString();
   }
 
   /**
-     * Return the base URI (external link written in NFT where pictures or others a stored)
-     * @return {string}
-     */
+   * Return the base URI (external link written in NFT where pictures or others a stored)
+   * @return {string}
+   */
   baseURI(): string {
     return this._baseURI;
   }
 
   /**
-     * Return the max supply
-     * @return {string}
-     */
+   * Return the max supply
+   * @return {string}
+   */
   limitSupply(): string {
     return this._limitSupply;
   }
 
   /**
-     * Return the current counter, if 10 NFT minted, returns 10.
-     * @return {u64}
-     */
+   * Return the current counter, if 10 NFT minted, returns 10.
+   * @return {u64}
+   */
   currentSupply(): string {
     return call(this._origin, 'currentSupply', '', 0);
   }
 
   /**
-     *
-     * Return the owner of a tokenID
-     * @param {u64} tokenId
-     * @return {string}
-     */
+   *
+   * Return the owner of a tokenID
+   * @param {u64} tokenId
+   * @return {string}
+   */
   ownerOf(tokenId: u64): string {
     return call(this._origin, 'ownerOf', tokenId.toString(), 0);
   }
 
   /**
-     *
-     * The to address becomes the owner of the next token (if current tokenID = 10, will mint 11 )
-     * Check if max supply is not reached
-     * @param {Address} to
-     * @return {string}
-     */
+   *
+   * The to address becomes the owner of the next token (if current tokenID = 10, will mint 11 )
+   * Check if max supply is not reached
+   * @param {Address} to
+   * @return {string}
+   */
   mint(to: Address): string {
     return call(this._origin, 'mint', to.toByteString(), 0);
   }
@@ -116,10 +116,8 @@ export class NFTWrapper {
     return call(
       this._origin,
       'transfer',
-      to
-        .toStringSegment()
-        .concat(ByteArray.fromU64(tokenId).toByteString()),
-      0
+      to.toStringSegment().concat(ByteArray.fromU64(tokenId).toByteString()),
+      0,
     );
   }
 }
