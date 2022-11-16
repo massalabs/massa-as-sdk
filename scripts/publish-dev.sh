8b2d3bc0e5c092557369b43075377c3757c4e8a5
Pierre Seznec <ps@massa.net>
2022-11-16 17:40:53 +0100

    disable husky in CI

--- scripts/publish-dev.sh
+++ scripts/publish-dev.sh
@@ -10,4 +10,8 @@ TIME=$(date -u +%Y%m%d%H%M%S)
 sed -i "/version/s/dev.0/dev.$TIME/g" package.json
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
 echo publishing @massalabs/massa-as-sdk@$PUBLISH_VERSION
+
+# disable husky
+npm pkg delete scripts.prepare
+
 npm publish --tag dev
