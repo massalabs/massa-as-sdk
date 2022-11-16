9ce13b84724d2341b0eaa65510283aa5c46c94ef
Trottoir <maxime.ranciere@2018.icam.fr>
2022-11-16 17:12:16 +0100

    Add callStack mocking

--- astester.imports.js
+++ astester.imports.js
@@ -2,23 +2,23 @@
 let exports;
 
 export function setExports(xpt) {
-  exports = xpt;
+    exports = xpt;
 }
 
 function mixRandomChars(length) {
-  let result = '';
-  let characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+    let result = '';
+    let characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 // Generates a 50 char lengh address
 function generateDumbAddress() {
-  return 'A12' + mixRandomChars(47);
+    return 'A12' + mixRandomChars(47);
 }
 
 /* Ledger format :
@@ -33,134 +33,133 @@ function generateDumbAddress() {
 }
 */
 export function local(memory) {
-  const defaultAddress = 'A12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq';
-  const contractAddress = 'A12BqZEQ6sByhRLyEuf0YbQmcF2PsDdkNNG1akBJu9XcjZA1eT';
+    const defaultAddress = 'A12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq';
+    const contractAddress = 'A12BqZEQ6sByhRLyEuf0YbQmcF2PsDdkNNG1akBJu9XcjZA1eT';
 
-  const ledger = new Map();
-  ledger.set(defaultAddress, {
-    storage: new Map(),
-    contract: '',
-  });
-  ledger.set(contractAddress, {
-    storage: new Map(),
-    contract: '',
-  });
+    const ledger = new Map();
+    ledger.set(defaultAddress, {
+        storage: new Map(),
+        contract: '',
+    });
+    ledger.set(contractAddress, {
+        storage: new Map(),
+        contract: '',
+    });
 
-  const SIZE_OFFSET = -4;
-  const utf16 = new TextDecoder('utf-16le', {fatal: true});
+    const SIZE_OFFSET = -4;
+    const utf16 = new TextDecoder('utf-16le', { fatal: true });
 
-  const getString = (ptr) => {
-    const len = new Uint32Array(memory.buffer)[(ptr + SIZE_OFFSET) >>> 2] >>> 1;
-    const wtf16 = new Uint16Array(memory.buffer, ptr, len);
-    return utf16.decode(wtf16);
-  };
+    const getString = (ptr) => {
+        const len = new Uint32Array(memory.buffer)[(ptr + SIZE_OFFSET) >>> 2] >>> 1;
+        const wtf16 = new Uint16Array(memory.buffer, ptr, len);
+        return utf16.decode(wtf16);
+    };
 
-  // from assemblyscript/loader/index
-  function newString(str) {
-    if (str == null) return 0;
-    const length = str.length;
-    const ptr = exports.__new(length << 1, 1);
-    const U16 = new Uint16Array(memory.buffer);
-    for (let i = 0, p = ptr >>> 1; i < length; ++i)
-      U16[p + i] = str.charCodeAt(i);
-    return ptr;
-  }
+    // from assemblyscript/loader/index
+    function newString(str) {
+        if (str == null) return 0;
+        const length = str.length;
+        const ptr = exports.__new(length << 1, 1);
+        const U16 = new Uint16Array(memory.buffer);
+        for (let i = 0, p = ptr >>> 1; i < length; ++i)
+            U16[p + i] = str.charCodeAt(i);
+        return ptr;
+    }
 
-  return {
-    massa: {
-      memory,
-      assembly_script_generate_event(string) {
-        console.log('event', getString(string));
-      },
+    return {
+        massa: {
+            memory,
+            assembly_script_generate_event(string) {
+                console.log('event', getString(string));
+            },
 
-      assembly_script_set_data(k_ptr, v_ptr) {
-        const k = getString(k_ptr);
-        const v = getString(v_ptr);
-        const addressStorage = ledger.get(contractAddress).storage;
-        addressStorage.set(k, v);
-      },
+            assembly_script_set_data(k_ptr, v_ptr) {
+                const k = getString(k_ptr);
+                const v = getString(v_ptr);
+                const addressStorage = ledger.get(contractAddress).storage;
+                addressStorage.set(k, v);
+            },
 
-      assembly_script_set_data_for(a_ptr, k_ptr, v_ptr) {
-        const a = getString(a_ptr);
-        const k = getString(k_ptr);
-        const v = getString(v_ptr);
-        if (!ledger.has(a)) {
-          ledger.set(a, {
-            storage: new Map(),
-            contract: '',
-          });
-        }
+            assembly_script_set_data_for(a_ptr, k_ptr, v_ptr) {
+                const a = getString(a_ptr);
+                const k = getString(k_ptr);
+                const v = getString(v_ptr);
+                if (!ledger.has(a)) {
+                    ledger.set(a, {
+                        storage: new Map(),
+                        contract: '',
+                    });
+                }
 
-        const addressStorage = ledger.get(a).storage;
-        addressStorage.set(k, v);
-      },
+                const addressStorage = ledger.get(a).storage;
+                addressStorage.set(k, v);
+            },
 
-      assembly_script_get_data(k_ptr) {
-        let v = '';
-        const k = getString(k_ptr);
-        if (ledger.has(contractAddress)) {
-          const addressStorage = ledger.get(contractAddress).storage;
-          if (addressStorage.has(k)) {
-            v = addressStorage.get(k);
-          }
-        }
-        return newString(v);
-      },
+            assembly_script_get_data(k_ptr) {
+                let v = '';
+                const k = getString(k_ptr);
+                if (ledger.has(contractAddress)) {
+                    const addressStorage = ledger.get(contractAddress).storage;
+                    if (addressStorage.has(k)) {
+                        v = addressStorage.get(k);
+                    }
+                }
+                return newString(v);
+            },
 
-      assembly_script_get_data_for(a_ptr, k_ptr) {
-        let v = '';
-        const a = getString(a_ptr);
-        const k = getString(k_ptr);
-        if (ledger.has(a)) {
-          const addressStorage = ledger.get(a).storage;
-          if (addressStorage.has(k)) {
-            v = addressStorage.get(k);
-          }
-        }
-        return newString(v);
-      },
+            assembly_script_get_data_for(a_ptr, k_ptr) {
+                let v = '';
+                const a = getString(a_ptr);
+                const k = getString(k_ptr);
+                if (ledger.has(a)) {
+                    const addressStorage = ledger.get(a).storage;
+                    if (addressStorage.has(k)) {
+                        v = addressStorage.get(k);
+                    }
+                }
+                return newString(v);
+            },
 
-      assembly_script_has_data(k_ptr) {
-        const k = getString(k_ptr);
-        const addressStorage = ledger.get(contractAddress).storage;
-        return addressStorage.has(k);
-      },
+            assembly_script_has_data(k_ptr) {
+                const k = getString(k_ptr);
+                const addressStorage = ledger.get(contractAddress).storage;
+                return addressStorage.has(k);
+            },
 
-      assembly_script_has_data_for(a_ptr, k_ptr) {
-        const a = getString(a_ptr);
-        const k = getString(k_ptr);
-        if (ledger.has(a)) {
-          const addressStorage = ledger.get(a).storage;
-          return addressStorage.has(k);
-        } else {
-          return false;
-        }
-      },
+            assembly_script_has_data_for(a_ptr, k_ptr) {
+                const a = getString(a_ptr);
+                const k = getString(k_ptr);
+                if (ledger.has(a)) {
+                    const addressStorage = ledger.get(a).storage;
+                    return addressStorage.has(k);
+                } else {
+                    return false;
+                }
+            },
 
-      assembly_script_get_call_stack() {
-        return newString(
-          '[ ' + defaultAddress + ' , ' + contractAddress + ' ]',
-        );
-      },
+            assembly_script_get_call_stack() {
+                return newString(
+                    '[ ' + defaultAddress + ' , ' + contractAddress + ' ]',
+                );
+            },
 
-      assembly_script_unsafe_random() {
-        const maxi64 = '9223372036854775807';
-        return BigInt(
-          Math.floor(BigInt(Math.random()) * BigInt(maxi64)).toString(),
-        );
-      },
+            assembly_script_unsafe_random() {
+                const maxi64 = '9223372036854775807';
+                return BigInt(
+                    Math.floor(BigInt(Math.random()) * BigInt(maxi64)).toString(),
+                );
+            },
 
-      // map the assemblyscript file with the contractAddresses generated
-      assembly_script_create_sc(path_ptr) {
-        const path = getString(path_ptr);
-        const newAddress = {_value: generateDumbAddress(), _isValid: true};
-        const newAddressLedger = {
-          storage: new Map(),
-          contract: path,
-        };
-        ledger.set(newAddress._value, newAddressLedger);
-        return newString(newAddress._value);
-      },
-    },
-  };
+            // map the assemblyscript file with the contractAddresses generated
+            assembly_script_create_sc(_) {
+                const newAddress = { _value: generateDumbAddress(), _isValid: true };
+                const newAddressLedger = {
+                    storage: new Map(),
+                    contract: path,
+                };
+                ledger.set(newAddress._value, newAddressLedger);
+                return newString(newAddress._value);
+            },
+        },
+    };
 }
