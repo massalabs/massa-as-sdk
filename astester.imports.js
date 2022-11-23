fc4f3a48c8b5070b4822db47e12353f20b04b356
Trottoir <maxime.ranciere@2018.icam.fr>
2022-11-23 10:41:11 +0100

    prettier

--- astester.imports.js
+++ astester.imports.js
@@ -2,13 +2,13 @@
 let exports;
 
 export function setExports(xpt) {
-    exports = xpt;
+  exports = xpt;
 }
 
 function mixRandomChars(length) {
   let result = '';
   let characters =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -44,24 +44,22 @@ export function local(memory) {
   function resetLedger() {
     ledger = new Map();
     ledger.set(callerAddress, {
-        storage: new Map(),
-        contract: '',
+      storage: new Map(),
+      contract: '',
     });
     ledger.set(contractAddress, {
-        storage: new Map(),
-        contract: '',
+      storage: new Map(),
+      contract: '',
     });
-  
   }
   const SIZE_OFFSET = -4;
-  const utf16 = new TextDecoder('utf-16le', { fatal: true });
+  const utf16 = new TextDecoder('utf-16le', {fatal: true});
 
-    const getString = (ptr) => {
-        const len =
-            new Uint32Array(memory.buffer)[(ptr + SIZE_OFFSET) >>> 2] >>> 1;
-        const wtf16 = new Uint16Array(memory.buffer, ptr, len);
-        return utf16.decode(wtf16);
-    };
+  const getString = (ptr) => {
+    const len = new Uint32Array(memory.buffer)[(ptr + SIZE_OFFSET) >>> 2] >>> 1;
+    const wtf16 = new Uint16Array(memory.buffer, ptr, len);
+    return utf16.decode(wtf16);
+  };
 
   // from assemblyscript/loader/index
   function newString(str) {
@@ -166,7 +164,7 @@ export function local(memory) {
       },
       // map the assemblyscript file with the contractAddresses generated
       assembly_script_create_sc(_) {
-        const newAddress = { _value: generateDumbAddress(), _isValid: true };
+        const newAddress = {_value: generateDumbAddress(), _isValid: true};
         const newAddressLedger = {
           storage: new Map(),
           contract: '',
