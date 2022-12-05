a2f516720f3050a62cefb67a882898b2bcf9301b
Nathan Seva <thykof@protonmail.ch>
2022-12-05 18:01:29 +0100

    remove vm-mock setData

--- assembly/vm-mock/storage.ts
+++ assembly/vm-mock/storage.ts
@@ -1,22 +1,5 @@
-// XXX : Is it used?
-
-/**
- * Set data in mocked storage
- *
- * Set the given value for the given key in mocked vm storage.
- *
- * @param {string} k - Key to set the data to.
- * @param {string} v - Value to set in storage.
- *
- * @returns {void}
- */
-// @ts-ignore: decorator
-@external("massa-sc-std", "set")
-export declare function setData(k: StaticArray<u8>, v: StaticArray<u8>): void;
-
 /**
  *  Change the callstack until the next call on this function
- *
  *
  * @param {string} callStack - string of addresses separated by a coma
  *
