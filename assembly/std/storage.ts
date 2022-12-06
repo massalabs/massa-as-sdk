0c2337191593cb8b13d2a74f2c1b4bd4b0a00ca0
Nathan Seva <thykof@protonmail.ch>
2022-12-06 11:46:19 +0100

    Update assembly/std/storage.ts
    Co-authored-by: Sebastien Valla <83454858+SebastienValla@users.noreply.github.com>

--- assembly/std/storage.ts
+++ assembly/std/storage.ts
@@ -144,7 +144,7 @@ export function del<T>(key: T): void {
  * Removes (key, value) from the datastore of the given address.
  *
  * TODO: explains what happens on missing key.
- * TODO: explains security mecanisms
+ * TODO: explains security mechanisms
  *
  * @template {string|Args|StaticArray<u8>} T - string, Args or StaticArray<u8>
  * @param {Address} address
