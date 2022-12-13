5acbb0a7c1f334c064587bcb7363adbcc59e1aef
Nathan Seva <thykof@protonmail.ch>
2022-12-13 14:27:48 +0100

    Fix vm mock for as-pect (#98)
    fixes vm mock and improves its readibility
    
    Co-authored-by: Grégory Libert <greg_libert@yahoo.fr>

--- vm-mock/vm.js
+++ vm-mock/vm.js
@@ -1,10 +1,4 @@
 /* eslint-disable require-jsdoc */
-/*
-XXX: Only the following ABI were tested:
- - assembly_script_get_data
- - assembly_script_set_data
-*/
-
 /**
  * Addresses and callstack
  */
@@ -16,7 +10,7 @@ const contractAddress = 'A12BqZEQ6sByhRLyEuf0YbQmcF2PsDdkNNG1akBJu9XcjZA1eT';
 function mixRandomChars(length) {
   let result = '';
   let characters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -24,7 +18,7 @@ function mixRandomChars(length) {
   return result;
 }
 
-// Generates a 50 chars lengh address
+// Generates a probably invalid address of 50 base58 characters
 function generateDumbAddress() {
   return 'A12' + mixRandomChars(47);
 }
@@ -50,12 +44,10 @@ let ledger;
 
 function resetLedger() {
   ledger = new Map();
-
   ledger.set(callerAddress, {
     storage: new Map(),
     contract: '',
   });
-
   ledger.set(contractAddress, {
     storage: new Map(),
     contract: '',
@@ -75,18 +67,40 @@ let webModule;
  * @returns {?} ?
  */
 export default function createMockedABI(memory, createImports, instantiate, binary) {
+  function byteArrToString(arr) {
+    return new TextDecoder("utf-8").decode(arr);
+  };
+
+  function ptrToString(ptr) {
+    return byteArrToString(webModule.__getArrayBuffer(ptr));
+  };
+
+  function ptrToUint8ArrayString(ptr) {
+    return new Uint8Array(webModule.__getArrayBuffer(ptr)).toString();
+  }
+
+  function getArrayBuffer(ptr) {
+    return webModule.__getArrayBuffer(ptr);
+  };
+
+  function newArrayBuffer(buffer) {
+    return webModule.__newArrayBuffer(buffer);
+  };
+
+  function newString(buffer) {
+    return webModule.__newString(buffer);
+  }
 
   resetLedger();
 
   const myImports = {
     massa: {
-      memory, // Do we need this ?
+      memory,
 
-      // Tested mocked ABI
 
       assembly_script_set_data(kPtr, vPtr) {
-        const k = new Uint8Array(webModule.__getArrayBuffer(kPtr)).toString();
-        const v = webModule.__getArrayBuffer(vPtr);
+        const k = ptrToUint8ArrayString(kPtr);
+        const v = getArrayBuffer(vPtr);
 
         const addressStorage = ledger.get(contractAddress).storage;
         addressStorage.set(k, v);
@@ -94,7 +108,7 @@ export default function createMockedABI(memory, createImports, instantiate, bina
 
       assembly_script_get_data(kPtr) {
         let v = '';
-        const k = new Uint8Array(webModule.__getArrayBuffer(kPtr)).toString();
+        const k = ptrToUint8ArrayString(kPtr);
 
         if (ledger.has(contractAddress)) {
           const addressStorage = ledger.get(contractAddress).storage;
@@ -104,32 +118,25 @@ export default function createMockedABI(memory, createImports, instantiate, bina
           }
         }
 
-        return webModule.__newArrayBuffer(v);
+        return newArrayBuffer(v);
       },
-
-      // XXX: Untested ABI, directly migrated from as-tester
-
-      // TODO: Please move the ABIs to the previous section once you have confirmed
-      //        that they are functional.
-      // This action can be done on the fly.
 
       assembly_script_reset_storage() {
         resetLedger();
       },
 
       assembly_script_change_call_stack(callstackPtr) {
-        const callStackToString = webModule.__getArrayBuffer(callstackPtr);
-        callStack = callStackToString;
+        callStack = ptrToString(callstackPtr);
       },
 
-      assembly_script_generate_event(string) {
-        console.log('event', webModule.__getArrayBuffer(string));
+      assembly_script_generate_event(msgPtr) {
+        console.log('event: ', ptrToString(msgPtr));
       },
 
       assembly_script_set_data_for(aPtr, kPtr, vPtr) {
-        const a = webModule.__getArrayBuffer(aPtr);
-        const k = webModule.__getArrayBuffer(kPtr);
-        const v = webModule.__getArrayBuffer(vPtr);
+        const a = ptrToString(aPtr);
+        const k = ptrToUint8ArrayString(kPtr);
+        const v = getArrayBuffer(vPtr);
         if (!ledger.has(a)) {
           ledger.set(a, {
             storage: new Map(),
@@ -141,30 +148,28 @@ export default function createMockedABI(memory, createImports, instantiate, bina
         addressStorage.set(k, v);
       },
 
-
-
       assembly_script_get_data_for(aPtr, kPtr) {
         let v = '';
-        const a = webModule.__getArrayBuffer(aPtr);
-        const k = webModule.__getArrayBuffer(kPtr);
+        const a = ptrToString(aPtr);
+        const k = ptrToUint8ArrayString(kPtr);
         if (ledger.has(a)) {
           const addressStorage = ledger.get(a).storage;
           if (addressStorage.has(k)) {
             v = addressStorage.get(k);
           }
         }
-        return webModule.__newArrayBuffer(v);
+        return newArrayBuffer(v);
       },
 
       assembly_script_has_data(kPtr) {
-        const k = webModule.__getArrayBuffer(kPtr);
+        const k = ptrToUint8ArrayString(kPtr);
         const addressStorage = ledger.get(contractAddress).storage;
         return addressStorage.has(k);
       },
 
       assembly_script_has_data_for(aPtr, kPtr) {
-        const a = webModule.__getArrayBuffer(aPtr);
-        const k = webModule.__getArrayBuffer(kPtr);
+        const a = ptrToString(aPtr);
+        const k = ptrToUint8ArrayString(kPtr);
         if (ledger.has(a)) {
           const addressStorage = ledger.get(a).storage;
           return addressStorage.has(k);
@@ -174,7 +179,7 @@ export default function createMockedABI(memory, createImports, instantiate, bina
       },
 
       assembly_script_get_call_stack() {
-        return webModule.__newArrayBuffer('[ ' + callStack + ']');
+        return newString('[ ' + callStack + ' ]');
       },
 
       assembly_script_unsafe_random() {
@@ -182,6 +187,7 @@ export default function createMockedABI(memory, createImports, instantiate, bina
           Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(),
         );
       },
+
       // map the assemblyscript file with the contractAddresses generated
       assembly_script_create_sc(_) {
         const newAddress = {_value: generateDumbAddress(), _isValid: true};
@@ -190,13 +196,15 @@ export default function createMockedABI(memory, createImports, instantiate, bina
           contract: '',
         };
         ledger.set(newAddress._value, newAddressLedger);
-        return webModule.__newArrayBuffer(newAddress._value);
+        return newArrayBuffer(newAddress._value);
       },
+
       assembly_script_get_time(){
         return BigInt(Date.now());
       }
     },
   };
+
   let instance = instantiate(binary, createImports(myImports));
 
   instance.then(function (result) {
