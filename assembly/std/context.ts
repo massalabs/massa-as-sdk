82b8d560b44027714531c73a3478bf581d0fe503
Nathan Seva <thykof@protonmail.ch>
2022-12-13 14:18:56 +0100

    improve doc of stack address (#91)
    improves stack addresses functions documentation
    
    
    
    Co-authored-by: Grégory Libert <greg_libert@yahoo.fr>

--- assembly/std/context.ts
+++ assembly/std/context.ts
@@ -33,9 +33,31 @@ export function ownedAddresses(): Array<Address> {
 /**
  * Returns stack addresses.
  *
- * The address stack is the list of the called modules' address.
- * You get all previously called since the address of
- * the originator (transaction creator).
+ * The first element of this list is the originator (creator of the transaction)
+ * and the last is the address of the current smart-contract.
+ *
+ * When executing the ExecuteSC function, the list is composed of only one entry:
+ * the initiator, the caller and the callee having with the same address.
+ *
+ * ExecuteSC: when an account A send an ExecuteSC operation, the stack at the
+ * beginning of that execution is: bottom [ A ] top.
+ *
+ * CallSC: when an account A sends a CallSC operation to call a function in a smart
+ * contract B, the stack at the beginning of the execution of that function is:
+ * bottom [ A, B ] top. Note: A and B can be the same
+ *
+ * Call from one smart contract to another: when a function F from smart contract C
+ * is being executed with the stack [A, B, C] and calls a function on a smart
+ * contract D, the stack at the beginning of the execution of D’s function becomes:
+ * bottom [A, B, C, D] top. When D’s function finishes, the stack becomes bottom [A, B, C]
+ * top and the execution of F resumes
+ *
+ * Autonomous SC: a message sent at a moment when the stack was [A, B, C] and calling a target
+ * function F of a smart contract D will yield the following stack at the beginning
+ * of the execution of the target function: bottom [C, D] top. Note: C and D can be the same
+ *
+ * Local execution (not yet implemented, see https://github.com/massalabs/massa-sc-runtime/issues/170):
+ * local executions don’t change the stack: they allow executing foreign code in the current context
  *
  * @return {Array<Address>}
  */
@@ -49,19 +71,19 @@ export function addressStack(): Array<Address> {
  * Caller is the person or the smart contract that directly called
  * the pending function.
  *
- * @return {Address}
+ * @return {Address} returns the stack element just below the top of the address stack
  */
 export function caller(): Address {
   const a = addressStack();
-  return a.length < 2 ? new Address('', false) : a[a.length - 2];
+  return a.length < 2 ? callee() : a[a.length - 2];
 }
 
 /**
  * Returns callee's address.
  *
- * Callee is the smart contract of the pending function.
+ * Callee is the current smart-contract address.
  *
- * @return {Address}
+ * @return {Address} returns the stack element at the top of the address stack
  */
 export function callee(): Address {
   const a = addressStack();
@@ -71,7 +93,7 @@ export function callee(): Address {
 /**
  * Return the address of the initial transaction creator (originator).
  *
- * @return {Address}
+ * @return {Address} returns the stack element at the bottom of the address stack
  */
 export function transactionCreator(): Address {
   return addressStack()[0];
