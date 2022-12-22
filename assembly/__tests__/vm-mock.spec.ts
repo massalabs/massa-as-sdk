33a516746072d2249cab0980e7d080bd0ac9dcc0
Nathan Seva <thykof@protonmail.ch>
2022-12-22 10:26:15 +0100

    improve test vm-mock

--- assembly/__tests__/vm-mock.spec.ts
+++ assembly/__tests__/vm-mock.spec.ts
@@ -2,94 +2,73 @@ import {
   Address,
   Storage,
   generateEvent,
-  Args,
   toBytes,
   Context,
   fromBytes,
+  Args,
 } from "../std";
+import { resetStorage } from "../vm-mock/storage";
 
 const testAddress = new Address(
   "A12E6N5BFAdC2wyiBV6VJjqkWhpz1kLVp2XpbRdSnL1mKjCWT6oR"
 );
 
-const keyTestArgs = new Args().add("test");
-const valueTestArgs = new Args().add("value");
+const keyTest = "test";
+const valueTest = "value";
 
-const keyTest = toBytes("test");
-const valueTest = toBytes("value");
+const keyTestArgs = new Args().add(keyTest);
+const valueTestArgs = new Args().add(valueTest);
+
+beforeEach(() => {
+  resetStorage();
+});
 
 describe("Testing mocked Storage and CallStack", () => {
   test("Testing the callstack", () => {
     // By convention addressStack returns always the two addresses.
     const callStack = Context.addressStack();
+
+    // expect to get the caller address in vm-mock/vm.js
     expect(callStack[0].toByteString()).toBe(
       "A12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq"
     );
+
+    // expect to get the contract address in vm-mock/vm.js
     expect(callStack[1].toByteString()).toBe(
       "A12BqZEQ6sByhRLyEuf0YbQmcF2PsDdkNNG1akBJu9XcjZA1eT"
     );
   });
 
-  test("Testing the Storage setOf", () => {
+  test("Testing the Storage setOf in bytes", () => {
     Storage.setOf(testAddress, keyTest, valueTest);
-    expect(
-      fromBytes(
-        Storage.getOf(
-          new Address("A12E6N5BFAdC2wyiBV6VJjqkWhpz1kLVp2XpbRdSnL1mKjCWT6oR"),
-          toBytes("test")
-        )
-      )
-    ).toBe("value");
+    expect(fromBytes(Storage.getOf(testAddress, toBytes(keyTest)))).toBe(
+      valueTest
+    );
   });
 
   test("Testing the Storage setOf with Args", () => {
     Storage.setOf(testAddress, keyTestArgs, valueTestArgs);
-    expect(
-      Storage.getOf(
-        new Address("A12E6N5BFAdC2wyiBV6VJjqkWhpz1kLVp2XpbRdSnL1mKjCWT6oR"),
-        new Args().add("test")
-      )
-        .nextString()
-        .unwrap()
-    ).toBe("value");
+    expect(Storage.getOf(testAddress, keyTestArgs).nextString().unwrap()).toBe(
+      valueTest
+    );
   });
 
-  test("Testing the Storage set", () => {
+  test("Testing the Storage get", () => {
     Storage.set(keyTest, valueTest);
-    expect(fromBytes(Storage.get(toBytes("test")))).toBe("value");
+    expect(Storage.get(keyTest)).toBe(valueTest);
   });
 
-  test("Testing the Storage set", () => {
+  test("Testing the Storage has", () => {
     Storage.set(keyTest, valueTest);
-    expect(Storage.has(toBytes("test"))).toBeTruthy();
+    expect(Storage.has(keyTest)).toBeTruthy();
   });
 
-  test("Testing the Storage set", () => {
-    Storage.set(keyTest, valueTest);
-    expect(
-      Storage.hasOf(
-        new Address("A12E6N5BFAdC2wyiBV6VJjqkWhpz1kLVp2XpbRdSnL1mKjCWT6oR"),
-        toBytes("test")
-      )
-    ).toBeTruthy();
+  test("Testing the Storage hasOf", () => {
+    Storage.setOf(testAddress, keyTest, valueTest);
+    expect(Storage.hasOf(testAddress, keyTest)).toBeTruthy();
   });
 
   test("Testing event", () => {
     generateEvent("I'm an event ");
   });
-});
-
-describe("Test Table tests", () => {
-  // prettier-ignore
-  checksForEachLineThatThe('sum of two integers', 'arg0 + arg1', is, 'arg3', [
-    1, 2, 3,
-    3, 4, 7,
-    4, 5, 9,
-  ]);
-
-  // prettier-ignore
-  checksForEachLineThatThe('`greater than` of two integers', 'arg0 > arg1', isFalse, [
-    0, 1,
-    2, 3,
-  ]);
 });
