6e30e5b1a427edae7411e5ccbd48c6c56d043be2
Ben <ben888@MacBook-Pro-de-Ben.local>
2023-02-03 15:29:01 +0100

    Add sha256 function

--- assembly/env/env.ts
+++ assembly/env/env.ts
@@ -181,4 +181,7 @@ export namespace env {
 
   @external("massa", "assembly_script_get_op_data")
   export declare function getOpData(key: StaticArray<u8>): StaticArray<u8>;
+
+  @external("massa", "assembly_script_sha256")
+  export declare function sha256(key: StaticArray<u8>): StaticArray<u8>;
 }
