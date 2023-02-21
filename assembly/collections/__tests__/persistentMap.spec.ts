c61216d797803bd13dde044fff3422e5ead38f4e
EvgeniPirianov <evgeni.pirianov@level01.io>
2023-02-22 00:25:53 +0200

    fixed hashmap tests

--- assembly/collections/__tests__/persistentMap.spec.ts
+++ assembly/collections/__tests__/persistentMap.spec.ts
@@ -3,9 +3,11 @@ import { PersistentMap } from '../persistentMap';
 describe('Persistent Map tests', () => {
   it('empty map', () => {
     const map = new PersistentMap<string, string>('my_map');
-    assert<bool>(map.size() === 0, 'empty map must have 0 size');
-    assert<bool>(!map.contains('some_key'), 'empty map contains no keys');
-    assert<bool>(!map.get('some_key'), 'empty map contains no keys');
+    expect<number>(map.size()).toBe(0, 'empty map must have 0 size');
+    expect(map.contains('some_key')).toBeFalsy('empty map contains no keys');
+    expect<string | null>(map.get('some_key')).toBeNull(
+      'empty map contains no keys',
+    );
   });
 
   it('basic map operations', () => {
@@ -17,47 +19,49 @@ describe('Persistent Map tests', () => {
 
     // set a value
     const setResult0 = map.set(key, value);
-    assert<bool>(setResult0.isOk(), `set should be OK`);
+    expect(setResult0.isOk()).toBeTruthy('set should be OK');
 
     // check map size
-    assert<bool>(map.size() === 1, 'size must be 1');
+    expect<number>(map.size()).toBe(1, 'size must be 1');
 
     // check for value
-    assert<bool>(map.contains(key), 'must contain key');
+    expect(map.contains(key)).toBeTruthy('must contain key');
 
     // get value
-    assert<string>(map.get(key) as string, value);
+    expect<string | null>(map.get(key)).toStrictEqual(value);
 
     // replace value
     const updatedValue: string = value.toUpperCase();
     const setResult = map.set(key, updatedValue);
-    assert<bool>(setResult.isOk(), `set should be OK`);
+    expect(setResult.isOk()).toBeTruthy('set should be OK');
 
     // check for value
-    assert<string>(map.get(key) as string, updatedValue);
+    expect<string | null>(map.get(key)).toStrictEqual(updatedValue);
 
     // check for value
-    assert<bool>(map.contains(key), 'must contain key');
+    expect(map.contains(key)).toBeTruthy('must contain key');
 
     // check map size
-    assert<bool>(map.size() === 1, 'size must be 1');
+    expect<number>(map.size()).toBe(1, 'size must be 1');
 
     // add another key now
     const key2 = 'some_other_key';
     const setResult2 = map.set(key2, updatedValue);
-    assert<bool>(setResult2.isOk(), `set should be OK`);
+    expect(setResult2.isOk()).toBeTruthy('set should be OK');
 
     // check map size again
-    assert<bool>(map.size() === 2, 'size must be 2');
+    expect<number>(map.size()).toBe(2, 'size must be 2');
 
     // delete value
     map.delete(key);
 
     // check map size again
-    assert<bool>(map.size() === 1, 'size must be 1');
+    expect<number>(map.size()).toBe(1, 'size must be 1');
 
     // key should not be there anymore
-    assert<bool>(!map.get(key), `key must have been deleted`);
+    expect<string | null>(map.get('some_key')).toBeNull(
+      'key must have been deleted',
+    );
   });
 
   it('uint8 values', () => {
@@ -68,16 +72,19 @@ describe('Persistent Map tests', () => {
       testArr[i] = 255;
     }
     const setResult = map.set(key, testArr);
-    assert<bool>(setResult.isOk(), `set should be OK`);
-    assert<bool>(map.size() === 1, 'size must be 1');
+    expect(setResult.isOk()).toBeTruthy('set should be OK');
+    expect<number>(map.size()).toBe(1, 'size must be 1');
     const value: Uint8Array = map.get(key) as Uint8Array;
-    assert<bool>(value !== null, 'retrieved value must not be null');
+    expect<Uint8Array | null>(value).not.toBeNull(
+      'retrieved value must not be null',
+    );
+    expect<Uint8Array | null>(map.get(key)).toStrictEqual(testArr);
     let isU8Set = true;
     for (let i = 0; i < value.length; i++) {
       if (testArr[i] !== 255) {
         isU8Set = false;
       }
     }
-    assert<bool>(isU8Set, `expected an array of u8s of 255`);
+    expect(isU8Set).toBeTruthy('expected an array of u8s of 255');
   });
 });
