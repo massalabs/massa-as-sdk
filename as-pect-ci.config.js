7d87319d0208a08607bf8eb72e3fcb05a636187a
Ben <br@massa.net>
2023-03-20 15:36:02 +0100

    [devops] reduce code by importing config

--- as-pect-ci.config.js
+++ as-pect-ci.config.js
@@ -1,23 +1,3 @@
-import createMockedABI from './vm-mock/vm.js';
-
-export default {
-  // A set of globs passed to the glob package that qualify typescript files for testing.
-  entries: ['**/assembly/**/*.spec.ts'],
-
-  // A set of globs passed to the glob package that quality files to be added to each test.
-  include: ['assembly/**/*.include.ts'],
-
-  // A set of regexp that will disclude source files from testing.
-  disclude: [/node_modules/],
-
-  // Add your required AssemblyScript imports here.
-  async instantiate(memory, createImports, instantiate, binary) {
-    return createMockedABI(memory, createImports, instantiate, binary);
-  },
-
-  // Enable code coverage.
-  coverage: ['assembly/**/*.ts'],
-
-  // Specify if the binary wasm file should be written to the file system.
-  outputBinary: false,
-};
+import config from './as-pect.config.js';
+config.coverage = ['assembly/**/*.ts'];
+export default config;
