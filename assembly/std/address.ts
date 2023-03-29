345611d728a8272db833643c6b8355db364b2355
Ben <br@massa.net>
2023-03-29 21:50:36 +0200

    [doc] Update address

--- assembly/std/address.ts
+++ assembly/std/address.ts
@@ -1,8 +1,7 @@
 import { Valider, Serializable, Args, Result } from '@massalabs/as-types';
 
 /**
- * A Massa's blockchain address.
- *
+ * Represents a Massa blockchain address.
  */
 export class Address implements Valider, Serializable {
   /**
@@ -14,32 +13,39 @@ export class Address implements Valider, Serializable {
   constructor(private _value: string = '', private _isValid: bool = true) {}
 
   /**
-   * Returns if the Address is still valid.
+   * Returns whether the Address is still valid.
    *
-   * see https://github.com/massalabs/massa-sc-runtime/issues/142
+   * @remarks
+   * This function checks whether the address is still valid, and returns a boolean value to indicate
+   * its status.
+   *
+   * @returns A boolean value indicating whether the address is still valid.
+   *
+   * @see https://github.com/massalabs/massa-sc-runtime/issues/142
    */
   isValid(): bool {
     return this._isValid;
   }
 
   /**
-   * Serialize the address
+   * Serialize the address.
    *
    * @remarks
-   * Addresses are not fixed-size so the first bytes are the size as a i32, then the address string is encoded.
+   * Addresses are not fixed-size, so the first bytes are the size as a `i32`, then the address string is encoded.
    *
-   * @returns the bytes
+   * @returns The serialized byte string of the address.
    */
   serialize(): StaticArray<u8> {
     return new Args().add(this._value).serialize();
   }
 
   /**
-   * Deserialize the address
+   * Deserialize the address.
    *
-   * @param data - bytes
-   * @param offset - `Args` instance current offset
-   * @returns the new offset wrapped in a `Result`
+   * @param data - The byte string to deserialize.
+   * @param offset - The current offset of the `Args` instance.
+   *
+   * @returns The new offset wrapped in a `Result`.
    */
   deserialize(data: StaticArray<u8>, offset: i32 = 0): Result<i32> {
     const args = new Args(data, offset);
@@ -52,7 +58,9 @@ export class Address implements Valider, Serializable {
   }
 
   /**
-   * Serialize to byte string.
+   * Convert the address to a string.
+   *
+   * @returns A string representation of the address.
    */
   toString(): string {
     return this._value;
@@ -61,20 +69,25 @@ export class Address implements Valider, Serializable {
   /**
    * Tests if two addresses are identical.
    *
-   * @param other - the address object to compare
+   * @param address - The address object to compare.
+   *
+   * @returns `true` if the addresses are identical, `false` otherwise.
    */
+
   @operator('==')
-  equals(other: Address): boolean {
-    return this._value == other.toString();
+  equals(address: Address): boolean {
+    return this._value == address.toString();
   }
 
   /**
    * Tests if two addresses are different.
    *
-   * @param other - the address object to compare
+   * @param address - The address object to compare.
+   *
+   * @returns `true` if the addresses are different, `false` otherwise.
    */
   @operator('!=')
-  notEqual(other: Address): boolean {
-    return !(this == other);
+  notEqual(address: Address): boolean {
+    return !(this == address);
   }
 }
