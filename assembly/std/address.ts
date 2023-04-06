5e4e1a6ebd6d08b84e643ee155da687ee59332b7
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-04-06 00:59:39 -0600

    Update assembly/std/address.ts
    Co-authored-by: Grégory Libert <gl@massa.net>

--- assembly/std/address.ts
+++ assembly/std/address.ts
@@ -103,6 +103,9 @@ export class Address implements Valider, Serializable {
    * This method compares the string representation of the current `Address` object to another `Address`
    * object to determine if they are different.
    *
+   * @remarks
+   * This method compares the string representation of the current `Address` object to another `Address` object to determine if they are identical.
+   *
    * @param address - The address object to compare.
    *
    * @returns `true` if the addresses are different, `false` otherwise.
