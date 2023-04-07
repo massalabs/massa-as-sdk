4423b661ebfbf25ea245ed06329f2bd65547ebed
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-04-07 06:56:19 -0600

    [documentation update] 193 documentation address class  (#197)
    Co-authored-by: Ben <br@massa.net>
    Co-authored-by: Benjamin Reynes <44082144+Ben-Rey@users.noreply.github.com>
    Co-authored-by: Grégory Libert <gl@massa.net>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- assembly/std/address.ts
+++ assembly/std/address.ts
@@ -1,45 +1,75 @@
+/**
+ * This module contains the 'Address' class, which represents a Massa blockchain address and provides helper features
+ * for easier manipulation and management of addresses.
+ *
+ * The 'Address' class provides methods to {@link serialize} and {@link deserialize} an address, as well
+ * as to convert it to a string with {@link toString}.
+ * It also provides overloading methods to test if two addresses are {@link equals} or {@link notEqual}.
+ *
+ * @module
+ */
+
 import { Valider, Serializable, Args, Result } from '@massalabs/as-types';
 
 /**
- * A Massa's blockchain address.
- *
+ * Represents a Massa blockchain address.
  */
 export class Address implements Valider, Serializable {
   /**
    * Creates a new Address;
    *
-   * @param bs - Byte string.
-   * @param isValid - default true
+   * @param _value - A byte string that represents the address data.
+   * @param _isValid - Defaults to `true`.
    */
   constructor(private _value: string = '', private _isValid: bool = true) {}
 
   /**
-   * Returns if the Address is still valid.
+   * Returns whether the Address is still valid.
    *
-   * see https://github.com/massalabs/massa-sc-runtime/issues/142
+   * @returns A boolean value indicating whether the address is still valid.
+   *
+   * @see https://github.com/massalabs/massa-sc-runtime/issues/142
    */
   isValid(): bool {
     return this._isValid;
   }
 
   /**
-   * Serialize the address
+   * Serialize the address.
    *
    * @remarks
-   * Addresses are not fixed-size so the first bytes are the size as a i32, then the address string is encoded.
+   * This method is used to serialize an Address object into a byte array,
+   * which can be passed as an argument to another smart contract's method,
+   * or stored in persistent storage.
    *
-   * @returns the bytes
+   * @privateRemarks
+   * Addresses base58check encoded are not fixed-size, so the first bytes are the size as a
+   * i32, then the address string is encoded.
+   *
+   * @returns The serialized byte string of the address.
    */
   serialize(): StaticArray<u8> {
     return new Args().add(this._value).serialize();
   }
 
   /**
-   * Deserialize the address
+   * Deserialize the address.
    *
-   * @param data - bytes
-   * @param offset - `Args` instance current offset
-   * @returns the new offset wrapped in a `Result`
+   * TODO: explain why we need this method
+   *
+   * @remarks
+   * This method deserializes a byte array into an `Address` object.
+   * Caller can test if the deserialization is successful by checking the
+   * [Result](https://as-types.docs.massa.net/classes/Result.html) wrapping the offset value.
+   * This offset can be used to identify where the next element starts in the byte array, if any.
+   *
+   * @privateRemarks
+   * This method constructs a new instance of the `Args` class using the `data` and `offset` parameters.
+   *
+   * @param data - The byte string to deserialize.
+   * @param offset - The offset of the current serialized address object in the byte array.
+   *
+   * @returns The offset of the next serialized object in the byte array wrapped in a `Result`.
    */
   deserialize(data: StaticArray<u8>, offset: i32 = 0): Result<i32> {
     const args = new Args(data, offset);
@@ -52,7 +82,9 @@ export class Address implements Valider, Serializable {
   }
 
   /**
-   * Serialize to byte string.
+   * Convert the address to a string.
+   *
+   * @returns A string representation of the address.
    */
   toString(): string {
     return this._value;
@@ -61,20 +93,37 @@ export class Address implements Valider, Serializable {
   /**
    * Tests if two addresses are identical.
    *
-   * @param other - the address object to compare
+   * @remarks
+   * This method compares the string representation of the current `Address` object to another `Address`
+   * object to determine if they are identical.
+   *
+   * @param address - The address object to compare.
+   *
+   * @returns `true` if the addresses are identical, `false` otherwise.
    */
+
   @operator('==')
-  equals(other: Address): boolean {
-    return this._value == other.toString();
+  equals(address: Address): boolean {
+    return this._value == address.toString();
   }
 
   /**
    * Tests if two addresses are different.
    *
-   * @param other - the address object to compare
+   * @remarks
+   * This method compares the string representation of the current `Address` object to another `Address`
+   * object to determine if they are different.
+   *
+   * @remarks
+   * This method compares the string representation of the current `Address` object to
+   * another `Address` object to determine if they are identical.
+   *
+   * @param address - The address object to compare.
+   *
+   * @returns `true` if the addresses are different, `false` otherwise.
    */
   @operator('!=')
-  notEqual(other: Address): boolean {
-    return !(this == other);
+  notEqual(address: Address): boolean {
+    return !(this == address);
   }
 }
