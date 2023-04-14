741dcb783b7e4d5e9d188af5c8a0049036308faf
Ben <br@massa.net>
2023-04-14 12:32:39 +0200

    [vm test] remove duplicate

--- vm-mock/vm.js
+++ vm-mock/vm.js
@@ -631,15 +631,6 @@ export default function createMockedABI(
           'AU12UBnqTHDQALpocVBnkPNy7y5CndUJQTLutaVDDFgMJcq5kQiKq',
         );
       },
-
-      assembly_script_local_call(_address, method, _param) {
-        if (scCallMockStack.length) {
-          return newArrayBuffer(scCallMockStack.shift());
-        }
-        throw new Error(
-          `No mock defined for sc call on "${ptrToString(method)}".`,
-        );
-      },
     },
   };
 
