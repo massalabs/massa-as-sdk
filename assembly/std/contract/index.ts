04f72933c1ba512468a2afb5cb7347633bd84da7
Ben <br@massa.net>
2023-04-20 11:07:51 +0200

    [archi] move setBytecode to contract

--- assembly/std/contract/index.ts
+++ assembly/std/contract/index.ts
@@ -22,6 +22,8 @@
  * In the file **bytecode.ts** you can find functions to manipulate **smart contracts bytecode** such as:
  * - {@link getBytecode}.
  * - {@link getBytecodeOf}.
+ * - {@link setBytecode}.
+ * - {@link setBytecodeOf}.
  * - {@link createSC}.
  *
  * @module
