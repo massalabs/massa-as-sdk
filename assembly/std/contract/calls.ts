4509e9e8171ae8af65fbda01814a4bdc34c44fc3
Benjamin Reynes <44082144+Ben-Rey@users.noreply.github.com>
2023-04-27 10:23:26 +0200

    [doc] improve localCall doc (#267)
    Co-authored-by: Grégory Libert <gl@massa.net>

--- assembly/std/contract/calls.ts
+++ assembly/std/contract/calls.ts
@@ -33,6 +33,11 @@ export function call(
  * Calls a function from a remote contract and execute it in the current context.
  *
  * @remarks
+ * When using localCall, the contract calls a function defined in another smart contract
+ * and executes it within its own context. This means that if a datastore key is modified
+ * in the called function, the modification will occur in the current smart contract's datastore.
+ * This behavior is attributed to the execution taking place "in the current context" of the caller.
+ *
  * Arguments serialization is to be handled by the caller and the callee.
  *
  * @param at - The address of the contract from where the function is located.
