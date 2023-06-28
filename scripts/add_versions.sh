44780df9aacd69a600849d76fad635984a6cf9d5
Ben <br@massa.net>
2023-06-28 10:12:21 +0200

    add dist version job

---
+++ scripts/add_versions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+PACKAGE_NAME=$(cat package.json | jq -r '.name')
+PUBLISH_VERSION=$(cat package.json | jq -r '.version')
+
+npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
+npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" testnet
