ffe21294a7e2439e1a42086aa0e0eaad57076ba9
Ben <br@massa.net>
2023-06-29 08:47:40 +0200

    remove hardcoded version

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -4,5 +4,5 @@ set -ex
 PACKAGE_NAME=$(cat package.json | jq -r '.name')
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
 
-npm dist-tag add "$PACKAGE_NAME@2.1.0" buildnet
-npm dist-tag add "$PACKAGE_NAME@2.1.0" testnet
+npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
+npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" testnet
