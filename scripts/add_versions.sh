ae86236f06a581ac7d3de2b63e427a02191e5357
Ben <br@massa.net>
2023-07-20 15:30:44 +0200

    trigger

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -4,5 +4,7 @@ set -ex
 PACKAGE_NAME=$(cat package.json | jq -r '.name')
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
 
+
 npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
 npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" testnet
+
