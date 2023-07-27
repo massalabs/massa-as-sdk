ab369345bde834fe9d4a8434fc8203d122709ff6
Ben <br@massa.net>
2023-07-27 17:06:52 +0200

    remove tag buildnet-dev

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -5,6 +5,6 @@ PACKAGE_NAME=$(cat package.json | jq -r '.name')
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
 
 
-npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
-npm dist-tag rm "$PACKAGE_NAME@2.3.0" testnet
-
+# npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
+# npm dist-tag rm "$PACKAGE_NAME@2.3.0" testnet
+npm dist-tag rm "$PACKAGE_NAME@2.1.1-dev.20230626132010" buildnet-dev
