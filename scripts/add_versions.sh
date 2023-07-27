e2dc6e2393461e0f2ae46560b31ec70b0457d558
Ben <br@massa.net>
2023-07-27 17:02:15 +0200

    update tag

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -6,5 +6,5 @@ PUBLISH_VERSION=$(cat package.json | jq -r '.version')
 
 
 npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
-npm rm "$PACKAGE_NAME@2.3.0" testnet
+npm dist-tag rm "$PACKAGE_NAME@2.3.0" testnet
 
