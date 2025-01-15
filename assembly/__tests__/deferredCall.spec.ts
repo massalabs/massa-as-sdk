c2dd03ed3913291e3054be2787f3f66397b71f74
Modship <yeskinokay@gmail.com>
2025-01-15 15:18:06 +0100

    Deferred calls (#356)
    * add deferred call abis
    
    * add deferred call vm-mocks
    
    ---------
    
    Co-authored-by: Pierre Seznec <petterseznec@gmail.com>

---
+++ assembly/__tests__/deferredCall.spec.ts
@@ -0,0 +1,36 @@
+import {
+  deferredCallCancel,
+  deferredCallExists,
+  deferredCallQuote,
+  deferredCallRegister,
+  Slot,
+} from '../std';
+
+describe('Deferred call test (vm mock)', () => {
+  it('exists', () => {
+    expect(deferredCallExists('dummy')).toBe(true);
+  });
+
+  it('cancel', () => {
+    deferredCallCancel('dummy');
+  });
+  it('quote', () => {
+    expect(deferredCallQuote(new Slot(123, 22), 123_456_789, 0)).toBe(
+      123456789,
+    );
+  });
+  it('register', () => {
+    expect(
+      deferredCallRegister(
+        'targetAddress',
+        'targetFunction',
+        new Slot(123, 22),
+        123_456_789,
+        [],
+        0,
+      ),
+    ).toBe(
+      'D12inbCjPBMwrsMoZbKPyzV3ZAKygPYi3JQ7K7myu5kxQFPvPNBv87FDRjdhs253B95',
+    );
+  });
+});
