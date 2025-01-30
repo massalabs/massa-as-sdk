d914bd53043973b4b05d8fd3181c42d7f8615c8f
sydhds <sydhds@gmail.com>
2025-01-30 11:06:54 +0100

    Add selfdestruct function

--- assembly/std/solidity_compat/misc.ts
+++ assembly/std/solidity_compat/misc.ts
@@ -1,5 +1,10 @@
 import { u256 } from 'as-bignum/assembly';
 import { wrapStaticArray } from '@massalabs/as-types';
+import { Address } from '../address';
+import { env } from '../../env';
+// import setBytecode = env.setBytecode;
+// import getKeys = env.getKeys;
+import { Storage } from '../index';
 
 // Convert bytes array (length 32) to u256
 // In Solidity, you can see code like:
@@ -9,4 +14,26 @@ import { wrapStaticArray } from '@massalabs/as-types';
 // }
 export function bytes32ToU256(a: StaticArray<u8>): u256 {
   return u256.fromUint8ArrayBE(wrapStaticArray(a));
+}
+
+// implementation Solidity selfdestruct:
+// https://www.infuy.com/blog/using-self-destruct-in-solidity-contracts/
+export function selfDestruct(transferToAddr: Address): void {
+  // 1- empty the SC
+  let emptySc = new StaticArray<u8>(0);
+  env.setBytecode(emptySc);
+
+  // 2- delete everything in Storage
+  let keys = env.getKeys(new StaticArray<u8>(0));
+  for (let i = 0; i < keys.length; i++) {
+    Storage.del(keys[i]);
+  }
+
+  // 3- transfer back coins if any
+  let scBalance = env.balance();
+  // Balance will most likely be > 0 as we deleted some keys from the Storage
+  // but if there is nothing in the Storage, no need to call transferCoins
+  if (scBalance > 0) {
+    env.transferCoins(transferToAddr.toString(), scBalance);
+  }
 }
